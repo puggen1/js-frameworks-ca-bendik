0b7d9e2bc735315ee5bcd498fab05cde32cf3fdd
flyinglawnmower <43915072+puggen1@users.noreply.github.com>
2023-03-31 16:22:42 +0200

    added resonsive design to cart page
    most of the design is good, but not fonts and some small elements

--- src/pages/Cart/style.js
+++ src/pages/Cart/style.js
@@ -2,11 +2,21 @@ import styled from "styled-components";
 import { Main } from "../../theme/theme";
 const CartMain = styled(Main)`
   width: 33%;
+  box-sizing: border-box;
   h1 {
     text-align: center;
   }
   .empty {
     text-align: center;
+  }
+  @media (max-width: 1024px) {
+    width: 75%;
+  }
+  @media (max-width: 745px) {
+    width: 90%;
+  }
+  @media (max-width: 585px) {
+    width: 100%;
   }
 `;
 const CartItems = styled.div`
@@ -25,12 +35,19 @@ const LowerCart = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 10px 0px;
+
+  @media (max-width: 585px) {
+    grid-template-columns: 1fr 2fr;
+  }
   .checkout {
     grid-column: 2 / 3;
     margin: 0 0 0 auto;
   }
   .removeAll {
     width: 50%;
+    @media (max-width: 585px) {
+      width: 100%;
+    }
   }
 `;
 
