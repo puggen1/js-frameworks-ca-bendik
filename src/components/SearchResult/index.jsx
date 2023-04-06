c634ddc349c9751a03eaebc08577089c43f06522
FlyingLawnmower <43915072+puggen1@users.noreply.github.com>
2023-04-06 13:37:36 +0200

    links fix

--- src/components/SearchResult/index.jsx
+++ src/components/SearchResult/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { SearchResultInner, SearchResultLink } from './style'
 const SearchResult = ({data}) => {
   return (
-    <SearchResultLink to={`product/` + data.id}>
+    <SearchResultLink to={`/product/` + data.id}>
         <SearchResultInner>
             <p>{data.title}</p>
             <p className='price'>{data.discountedPrice},- </p>
