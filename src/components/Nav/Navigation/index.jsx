0ba1924e05bdaa0f57ba8b63f7bd7f3ed0689bb2
FlyingLawnmower <43915072+puggen1@users.noreply.github.com>
2023-04-10 10:48:08 +0200

    closing modal when nav item on mobile is clicked

--- src/components/Nav/Navigation/index.jsx
+++ src/components/Nav/Navigation/index.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { NavModalContext } from '../../../context/NavModal';
 import { NavSection, ModalNavSection } from './index.styles'
 import "../../../theme/style.css";
 /**
@@ -8,17 +9,18 @@ import "../../../theme/style.css";
  * @returns the nav content, based on the state
  */
 const Navigation = ({modal}) => {
+  const {setModalStatus} = useContext(NavModalContext);
   return (
     modal
     ?
     <ModalNavSection>
-        <Link className='defaultLink' to="/">Home</Link>
-        <Link className='defaultLink' to="/contact">Contact</Link>    
+        <Link className='defaultLink' onClick={()=>{setModalStatus(false)}} to="/">Home</Link>
+        <Link className='defaultLink' onClick={()=>{setModalStatus(false)}} to="/contact">Contact</Link>    
     </ModalNavSection>
     :  
     <NavSection>
-        <Link className='defaultLink' to="/">Home</Link>
-        <Link className='defaultLink' to="/contact">Contact</Link>
+        <Link className='defaultLink' onClick={()=>{setModalStatus(false)}} to="/">Home</Link>
+        <Link className='defaultLink' onClick={()=>{setModalStatus(false)}} to="/contact">Contact</Link>
     </NavSection>
   
   )
